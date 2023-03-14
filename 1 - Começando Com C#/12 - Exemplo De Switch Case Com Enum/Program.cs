104d4ad4e1e8498fa8d76adc73a8809ab9c299c3
TiagoGalhoto <tiagogalhoto@2gmail.com>
2023-03-14 13:59:57 -0300

    Estudo

---
+++ 1 - Começando Com C#/12 - Exemplo De Switch Case Com Enum/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace CondicionalEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Teste = 1;
+            switch (Teste)
+            {
+                case 1:
+                    Console.WriteLine("Um...!!!");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Dois...!!!");
+                    break;
+                default:
+                    Console.WriteLine("Não encontrou nada...!!!");
+                    break;
+
+            }
+        }
+    }   
+}
