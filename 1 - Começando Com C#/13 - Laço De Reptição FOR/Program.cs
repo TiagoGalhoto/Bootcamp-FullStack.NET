bd548475ba2d687602355552fd68fd664d6f4acb
TiagoGalhoto <tiagogalhoto@2gmail.com>
2023-03-16 18:45:47 -0300

    Estudo

---
+++ 1 - Começando Com C#/13 - Laço De Reptição FOR/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LacoFor
+{
+    class Progran
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World...!!!");
+            for (var x = 0; x < 10; x++)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
