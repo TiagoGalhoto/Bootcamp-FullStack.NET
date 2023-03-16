e62b93e7095d78a8b7c836990f7983aaff0b072a
TiagoGalhoto <tiagogalhoto@2gmail.com>
2023-03-16 19:22:18 -0300

    Estudo

---
+++ 1 - Começando Com C#/14 - Laço DO WHILE/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LacoDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 0;
+            do
+            {
+                Console.WriteLine("Teste...!!!");
+                x++;
+            }
+            while (x < 10);
+        }
+    }
+}
