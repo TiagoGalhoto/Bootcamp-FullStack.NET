e7b301c5554050d9d809ed0e864d0931acac5c8a
TiagoGalhoto <tiagogalhoto@2gmail.com>
2023-03-20 12:46:34 -0300

    Estudo

--- 1 - Começando Com C#/13 - Laço De Reptição FOR/Program.cs
+++ 1 - Começando Com C#/13 - Laço De Reptição FOR/Program.cs
@@ -4,7 +4,6 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World...!!!");
             for (var x = 0; x < 10; x++)
             {
                 Console.WriteLine(x);
