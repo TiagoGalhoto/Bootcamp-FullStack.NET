456fa1e54dd878d272c8a8d2e4dda74de04b20c6
TiagoGalhoto <tiagogalhoto@2gmail.com>
2023-03-21 21:46:17 -0300

    Estudo

---
+++ 1 - Começando Com C#/16 - Função/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Funcao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var order = new Order();
+            order.Place();
+            Console.WriteLine(order.Place(amount: 10.5M));
+            
+        }
+    }
+    public class Order
+    {
+        public string Place(decimal amount)
+        {
+            return $"XPTO00123456 {amount}";
+        }
+        public void Place()
+        {
+            Console.WriteLine("XPTO00123456");
+        }
+    }
+
+}
