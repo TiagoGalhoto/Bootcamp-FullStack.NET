8a66b2d6afcfe21cb8d53351f0d22c00d104a160
TiagoGalhoto <tiagogalhoto@2gmail.com>
2023-03-30 15:49:13 -0300

    Estudo

--- 2 - Entity Framework/1 - EFCoreCrud/Models/Todo.cs
+++
@@ -1,8 +0,0 @@
-namespace EfCoreCrud.Models;
-public class Todo
-{
-    public int Id {get; set;}
-    public string Title {get; set;}
-    public DateTime CreatedAt {get; set;} = DateTime.Now;
-    public bool Done {get; set;}
-}
